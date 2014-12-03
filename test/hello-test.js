6c8790ccb180a58a808c42353d48969a9b4a7b67
Alex Birch <alexander.birch@billforward.net>
2014-12-03 13:44:19 +0000

    Something like a module

--- test/hello-test.js
+++ test/hello-test.js
@@ -1,4 +1,4 @@
-var Hello = require('../lib').Hello;
+var BillForward = require('../lib');
 var sinon = require("sinon");
 var assert = require('assert');
 
@@ -6,11 +6,11 @@ describe('Hello', function () {
     // An example of synchronous testing in Mocha.
     describe('#sayHello', function () {
         it('should return the default hello message if no options are provided', function () {
-            var hello = new Hello();
+            var hello = new BillForward.Hello();
             assert.equal('Hello World!', hello.sayHello());
         });
         it('should allow the user to override the helloMessage when instantiating', function () {
-            var hello = new Hello({
+            var hello = new BillForward.Hello({
                 helloMessage: '你好世界'
             });
             assert.equal('你好世界', hello.sayHello());
@@ -20,7 +20,7 @@ describe('Hello', function () {
     // An example of asynchronous testing in Mocha.
     describe('#sayHelloLater', function () {
         it('should execute callback with the hello message asynchronously', function (done) {
-            var hello = new Hello();
+            var hello = new BillForward.Hello();
             hello.sayHelloLater(function (helloMessage) {
                 assert.equal(helloMessage, 'Hello World!');
                 // calling done indicates that all asynchronous
@@ -33,7 +33,7 @@ describe('Hello', function () {
     // An example of using sinon to spy on function calls.
     describe('#sayHelloThenSayHelloLater', function () {
         it('should invoke both #sayHello and #sayHelloLater', function () {
-            var hello = new Hello();
+            var hello = new BillForward.Hello();
             // stub the sayHello and sayHelloLater methods
             // with sinon.
             hello.sayHello = sinon.spy();
