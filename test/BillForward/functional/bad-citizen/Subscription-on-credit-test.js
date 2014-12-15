61095655fe91194ebd95e0cb41ef4cc21fce22af
Alex Birch <alexander.birch@billforward.net>
2014-12-15 12:39:16 +0000

    more syntax experimentation
    need to make certain promises dependent and serial..

--- test/BillForward/functional/bad-citizen/Subscription-on-credit-test.js
+++ test/BillForward/functional/bad-citizen/Subscription-on-credit-test.js
@@ -9,28 +9,63 @@ context(testBase.getContext(), function () {
 
 				var promise;
 				before(function() {
-					var accountModel = new BillForward.Account({
-						'profile': new BillForward.Profile({
-							'email': 'u.n.owen@was.her',
-		  					'firstName': 'U.N.',
-		  					'lastName': 'Owen',
-							'addresses': [
-								new BillForward.Address({
-								    'addressLine1': 'address line 1',
-								    'addressLine2': 'address line 2',
-								    'addressLine3': 'address line 3',
-								    'city': 'London',
-								    'country': 'Gensokyo',
-								    'province': 'London',
-								    'postcode': 'SW1 1AS',
-								    'landline': '02000000000',
-								    'primaryAddress': true
-								})
-								]
-							})
-						});
+					var models = {};
+					var promises = {};
 
-					var account = BillForward.Account.create(accountModel);
+					// create an account
+					// requires (optionally):
+					// - profile
+					// - - addresses
+					models.address = new BillForward.Address({
+					    'addressLine1': 'address line 1',
+					    'addressLine2': 'address line 2',
+					    'addressLine3': 'address line 3',
+					    'city': 'London',
+					    'country': 'Gensokyo',
+					    'province': 'London',
+					    'postcode': 'SW1 1AS',
+					    'landline': '02000000000',
+					    'primaryAddress': true
+					});
+					models.profile = new BillForward.Profile({
+						'email': 'u.n.owen@was.her',
+	  					'firstName': 'U.N.',
+	  					'lastName': 'Owen',
+						'addresses': [models.address]
+					});
+					models.account = new BillForward.Account({
+						profile: profileModel
+					});
+
+					promises.account = BillForward.Account.create(models.account);
+
+					models.creditNote = new BillForward.CreditNote({
+						'accountID': account.id,
+						'nominalValue': 100,
+						'currency': 'USD'
+					});
+
+					promises.creditNote = BillForward.CreditNote.create(models.creditNote);
+
+					// create a unit of measure
+					models.unitOfMeasure1 = new BillForward.UnitOfMeasure({
+						'name': 'CPU',
+						'displayedAs': 'Cycles',
+						'roundingScheme': 'UP',
+					});
+
+					promises.unitOfMeasure1 = BillForward.UnitOfMeasure.create(models.unitOfMeasure1)
+
+					// create another unit of measure
+					models.unitOfMeasure2 = new BillForward.UnitOfMeasure({
+						'name': 'Bandwidth',
+						'displayedAs': 'Mbps',
+						'roundingScheme': 'UP',
+					});
+					
+					promises.unitOfMeasure2 = BillForward.UnitOfMeasure.create(models.unitOfMeasure2)
+
+					
 
 					promise = account;
 				});
