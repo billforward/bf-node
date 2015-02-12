73967f39f0f93d45f85de49afeb8f070172a45b0
Alex Birch <alexander.birch@billforward.net>
2015-02-12 12:30:51 +0000

    ugh

--- lib/BillForward/Entity/Coupon/backend/AddCouponCodeRequest.ts
+++ lib/BillForward/Entity/Coupon/backend/AddCouponCodeRequest.ts
@@ -23,7 +23,7 @@ module BillForward {
 
 		    	var endpoint = Imports.util.format("%s/coupons", encodeURIComponent(subscriptionIdentifier));
 
-		    	var responseEntity = Coupon.constructor;
+		    	var responseEntity = new Coupon();
 		    	var client = requestEntity.getClient();
 
 		    	var myClass = this.getDerivedClassStatic();
